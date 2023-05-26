e7a924060b73410f68702a3ef638e1000003b18b
Smalick0478 <sameermalik0478@gmail.com>
2023-05-26 21:05:02 +0500

    Admin, Driver & Parent Dashboards Added.

---
+++ lib/screens/dashboards/driver.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../../pages/login_page.dart';
+
+class Driver extends StatefulWidget {
+  @override
+  State<Driver> createState() => _DriverState();
+}
+
+class _DriverState extends State<Driver> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Driver"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              logout(context);
+            },
+            icon: Icon(
+              Icons.logout,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    CircularProgressIndicator();
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LoginPage(),
+      ),
+    );
+  }
+}
