cdffc7406273e13af0f271e8b1aac8d8239afbe4
Smalick0478 <sameermalik0478@gmail.com>
2023-05-31 00:29:19 +0500

    Internet Connectivity Check Added.

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -5,6 +5,7 @@ import 'package:wheelie/components/main_button.dart';
 import 'package:wheelie/helpers/font_size.dart';
 import 'package:wheelie/helpers/theme_colors.dart';
 import 'package:wheelie/pages/login_page.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class StartPage extends StatefulWidget {
   const StartPage({Key? key}) : super(key: key);
@@ -14,85 +15,127 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPageState extends State<StartPage> {
+  Stream<InternetConnectionStatus> connectionStream =
+      InternetConnectionChecker().onStatusChange;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Image(
-            image: AssetImage('images/bg.png'),
-            height: MediaQuery.of(context).size.height,
-            fit: BoxFit.cover,
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Colors.transparent,
-                  Colors.transparent,
-                  ThemeColors.scaffoldBgColor,
-                ],
+      body: StreamBuilder<InternetConnectionStatus>(
+        stream: connectionStream,
+        initialData: InternetConnectionStatus.connected,
+        builder: (BuildContext context,
+            AsyncSnapshot<InternetConnectionStatus> snapshot) {
+          bool isConnected =
+              snapshot.data == InternetConnectionStatus.connected;
+          return Stack(
+            children: [
+              Image(
+                image: AssetImage('images/bg.png'),
+                height: MediaQuery.of(context).size.height,
+                fit: BoxFit.cover,
               ),
-            ),
-          ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons
-                              .directions_bus, // Replace with the desired bus icon
-                          size: 30,
-                          color: Color.fromARGB(255, 248, 215, 68),
-                        ),
-                        Text(
-                          'Wheelie',
-                          style: GoogleFonts.poppins(
-                            color: Color.fromARGB(255, 248, 215, 68),
-                            fontSize: 35,
-                            fontWeight: FontWeight.w600,
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                      ],
-                    ),
+              Container(
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.transparent,
+                      Colors.transparent,
+                      ThemeColors.scaffoldBgColor,
+                    ],
                   ),
-                  Spacer(),
-                  Text(
-                    'Track Your Van\nTo Ensure Your Child Is Safe.',
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.poppins(
-                      color: ThemeColors.whiteTextColor,
-                      fontSize: FontSize.medium,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 25),
-                    child: MainButton(
-                      onTap: () => Navigator.pushReplacement(
-                        context,
-                        CupertinoPageRoute(
-                          builder: (context) => LoginPage(),
+                ),
+              ),
+              SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.directions_bus,
+                              size: 30,
+                              color: Color.fromARGB(255, 248, 215, 68),
+                            ),
+                            Text(
+                              'Wheelie',
+                              style: GoogleFonts.poppins(
+                                color: Color.fromARGB(255, 248, 215, 68),
+                                fontSize: 35,
+                                fontWeight: FontWeight.w600,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ],
                         ),
-                        result: false,
                       ),
-                      text: 'Get Started',
-                      backgroundColor: Color.fromARGB(255, 248, 215, 68),
-                    ),
+                      Spacer(),
+                      Text(
+                        'Track Your Van\nTo Ensure Your Child Is Safe.',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                          color: ThemeColors.whiteTextColor,
+                          fontSize: FontSize.medium,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 25),
+                        child: MainButton(
+                          onTap: isConnected
+                              ? () => Navigator.pushReplacement(
+                                    context,
+                                    CupertinoPageRoute(
+                                      builder: (context) => LoginPage(),
+                                    ),
+                                  )
+                              : () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                      backgroundColor: Colors.black,
+                                      title: Row(
+                                        children: [
+                                          Icon(Icons.close, color: Colors.red),
+                                          SizedBox(width: 8),
+                                          Text('No Internet Connection',
+                                              style: TextStyle(
+                                                  color: Colors.white)),
+                                        ],
+                                      ),
+                                      content: Text(
+                                        'Please check your internet connection and try again.',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: Text('OK',
+                                              style: TextStyle(
+                                                  color: Colors.white)),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                },
+                          text: 'Get Started',
+                          backgroundColor: Color.fromARGB(255, 248, 215, 68),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
-          ),
-        ],
+            ],
+          );
+        },
       ),
     );
   }
