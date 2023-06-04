5bcd9bbba5bc516a4f52016a7a8b2c9d92f4aa3e
Smalick0478 <sameermalik0478@gmail.com>
2023-06-04 21:52:20 +0500

    fetech username of a logged in user

--- lib/screens/dashboards/admin/admin.dart
+++ lib/screens/dashboards/admin/admin.dart
@@ -2,9 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:wheelie/helpers/current_logged_person.dart';
 import 'package:wheelie/helpers/theme_colors.dart';
+import 'package:wheelie/pages/login_page.dart';
 
-//Admin
+// Admin
 import 'package:wheelie/screens/dashboards/admin/admin_home.dart';
 import 'package:wheelie/screens/dashboards/admin/profile.dart';
 import 'package:wheelie/screens/dashboards/admin/registered_driver.dart';
@@ -17,6 +19,7 @@ class AdminDashboard extends StatefulWidget {
 
 class _AdminDashboardState extends State<AdminDashboard> {
   int _currentIndex = 0;
+  String adminName = ''; // Variable to store the admin's name
 
   final List<Widget> _screens = [
     HomeScreen(),
@@ -24,6 +27,16 @@ class _AdminDashboardState extends State<AdminDashboard> {
     RegisteredParentScreen(),
     ProfileScreen(),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCurrentUserName().then((name) {
+      setState(() {
+        adminName = name;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +47,90 @@ class _AdminDashboardState extends State<AdminDashboard> {
         backgroundColor: ThemeColors
             .YellowColor, // Set the app bar background color to yellow
         foregroundColor: Colors.black, // Set the app bar text color to black
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: ThemeColors.YellowColor,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Wheelie',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(height: 8), // Add some spacing between the texts
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Text(
+                        'UserName: $adminName',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 0;
+                  Navigator.pop(context);
+                });
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.supervised_user_circle_outlined),
+              title: Text('Registered Drivers'),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 1;
+                  Navigator.pop(context);
+                });
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.supervised_user_circle_outlined),
+              title: Text('Registered Parents'),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 2;
+                  Navigator.pop(context);
+                });
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Profile'),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 3;
+                  Navigator.pop(context);
+                });
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () {
+                logout(context);
+              },
+            ),
+          ],
+        ),
       ),
       body: _screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
@@ -67,6 +164,17 @@ class _AdminDashboardState extends State<AdminDashboard> {
             label: 'Profile',
           ),
         ],
+      ),
+    );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    CircularProgressIndicator();
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LoginPage(),
       ),
     );
   }
