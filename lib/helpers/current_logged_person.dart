3d0a64d2bc3aad5ca32309eb39ebf228d16fc8f6
Smalick0478 <sameermalik0478@gmail.com>
2023-06-15 00:38:03 +0500

    retrieve current logged in user email, u.id and name

--- lib/helpers/current_logged_person.dart
+++ lib/helpers/current_logged_person.dart
@@ -1,7 +1,30 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-Future<String> fetchCurrentUserName() async {
+Stream<String> fetchCurrentUserName() {
+  final user = FirebaseAuth.instance.currentUser;
+
+  if (user != null) {
+    final userDoc =
+        FirebaseFirestore.instance.collection('users').doc(user.uid);
+    final useremail =
+        FirebaseFirestore.instance.collection('users').doc(user.email);
+    final userphone =
+        FirebaseFirestore.instance.collection('users').doc(user.phoneNumber);
+
+    return userDoc.snapshots().map((snapshot) {
+      if (snapshot.exists) {
+        return snapshot['Name'];
+      } else {
+        return 'Unknown';
+      }
+    });
+  }
+
+  return Stream.value('Unknown');
+}
+
+Future<String> fetchCurrentUserEmail() async {
   final user = FirebaseAuth.instance.currentUser;
 
   if (user != null) {
@@ -11,9 +34,19 @@ Future<String> fetchCurrentUserName() async {
         .get();
 
     if (userData.exists) {
-      return userData['Name'];
+      return userData['email'];
     }
   }
 
   return 'Unknown'; // Return 'Unknown' if user or user data doesn't exist
+}
+
+Future<String> fetchCurrentUserID() async {
+  final user = FirebaseAuth.instance.currentUser;
+
+  if (user != null) {
+    return user.uid;
+  }
+
+  return 'Unknown'; // Return 'Unknown' if user is null
 }
