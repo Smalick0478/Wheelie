fd3bef678e3125032d12a8ea6b6541b5a23dd9f5
Smalick0478 <sameermalik0478@gmail.com>
2023-06-15 00:50:37 +0500

    file created.

---
+++ lib/screens/dashboards/driver/parents_request.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ParentsRequest extends StatefulWidget {
+  const ParentsRequest({super.key});
+
+  @override
+  State<ParentsRequest> createState() => _ParentsRequestState();
+}
+
+class _ParentsRequestState extends State<ParentsRequest> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
