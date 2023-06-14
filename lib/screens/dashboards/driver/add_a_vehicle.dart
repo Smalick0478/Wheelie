086908b149530e6a4b34b56e8c5d2192e2fe46c5
Smalick0478 <sameermalik0478@gmail.com>
2023-06-15 00:49:47 +0500

    file created.

---
+++ lib/screens/dashboards/driver/add_a_vehicle.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddAVehicle extends StatefulWidget {
+  const AddAVehicle({super.key});
+
+  @override
+  State<AddAVehicle> createState() => _AddAVehicleState();
+}
+
+class _AddAVehicleState extends State<AddAVehicle> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
